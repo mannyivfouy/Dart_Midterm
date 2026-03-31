826e70da9f737e9a9259b8c938832b78cfc0ff74
Mann Yivfouy <mannyivfouy@gmail.com>
2026-03-31 15:16:08 +0700

    declare variable

--- bin/dart_midterm.dart
+++ bin/dart_midterm.dart
@@ -1,3 +1,14 @@
-void main(){
-  print("First Run");
+import 'dart:io';
+import 'package:intl/intl.dart';
+
+void main() {
+  double loanMoney = 1000;
+  double loanRate = 1.50;
+  int loanTerm = 12;
+  DateTime loanDate = DateTime.parse('2024-08-13');
+  String formatDate = DateFormat('yyyy-MM-dd').format(loanDate);
+  
+  
+  print("Loan Money : \$$loanMoney".padRight(30) + "Loan Rate : %$loanRate");
+  print("Loan Date  : $formatDate".padRight(30) + "Loan Term : $loanTerm");
 }
